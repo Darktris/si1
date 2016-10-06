519aae736929b807e33ca234bd9ec5ef474433b0
Serede Sixty Six <serede.dev@gmail.com>
2016-10-06 15:19:52 +0200

    DUDUDUDU

--- E1_Daniel_Perdices_Sergio_Fuentes/form1.php
+++ E1_Daniel_Perdices_Sergio_Fuentes/form1.php
@@ -13,14 +13,14 @@ if (isset($_REQUEST["submit2"])) {
     $resto = $final-$_SESSION["edad"];
     echo "Estimad".$_SESSION["sufijo"]." ".$_SESSION["tratamiento"].$_SESSION["nombre"].". Le informamos que la edad media de finalización de estos estudios es ".$final." años.".($resto > 0 ? " Puede terminar por tanto en ".$resto." años." : " Debería de haber terminado por tanto hace ".(-$resto)." años. Su queja al departamento de Circuitos Electrónicos está siendo tramitada en estos instantes.");
 }  else if (isset($_REQUEST["submit1"])) {
+    $_SESSION["nombre"] =  $_REQUEST["nombre"];
+    $_SESSION["edad"] =  $_REQUEST["edad"];
+    $_SESSION["genero"] =  $_REQUEST["genero"];
+    $_SESSION["tratamiento"] = $_SESSION["genero"] == "Hombre" ? "Sr. " : ($_SESSION["genero"] == "Mujer" ? "Srta. " : "bicho llamado ");
+    $_SESSION["sufijo"] = $_SESSION["genero"] == "Hombre" ? "o" : ($_SESSION["genero"] == "Mujer" ? "a" : "erino");
     if (!isset($_REQUEST["eps"])) {
         echo "Por favor, ".$_SESSION["tratamiento"].$_REQUEST["nombre"]." debe acceder a la página Web de la Universidad para seguir con sus gestiones.";
     } else {
-        $_SESSION["nombre"] =  $_REQUEST["nombre"];
-        $_SESSION["edad"] =  $_REQUEST["edad"];
-        $_SESSION["genero"] =  $_REQUEST["genero"];
-        $_SESSION["tratamiento"] = $_SESSION["genero"] == "Hombre" ? "Sr. " : ($_SESSION["genero"] == "Mujer" ? "Srta. " : "bicho llamado ");
-        $_SESSION["sufijo"] = $_SESSION["genero"] == "Hombre" ? "o" : ($_SESSION["genero"] == "Mujer" ? "a" : "erino");
         echo "Bienvenid".$_SESSION["sufijo"]." ".$_SESSION["tratamiento"].$_REQUEST["nombre"].", seleccione del siguiente listado el nombre de la titulación que está cursando:<br><br>";
         echo "<form action=\"form1.php\" method=\"POST\">";
         echo "<datalist id=\"grados\">";
