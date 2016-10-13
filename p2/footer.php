89c7d7b91b5ef97fe4b3638c2c1ad04567a1846e
Serede Sixty Six <serede.dev@gmail.com>
2016-10-13 12:50:48 +0200

    Restructured to avoid repeated code.

---
+++ p2/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0!"/>
+    <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="¡CSS Válido!"/>
+</footer>
---
+++ p2/mywallet.php
@@ -0,0 +1,10 @@
+<div id="scrollable">
+    <div class="title">Current credit:</div>
+    <div class="category">1.000.000 €</div>
+    <div class="option">
+        Charge credit
+    </div>
+    <div class="option">
+        Withdraw credit
+    </div>
+</div>
