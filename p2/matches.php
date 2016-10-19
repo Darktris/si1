27f2b5446319315930dc823ff25aeb28340a00f5
Serede Sixty Six <serede.dev@gmail.com>
2016-10-19 11:28:37 +0200

    Order matches by date.

--- p2/matches.php
+++ p2/matches.php
@@ -2,58 +2,61 @@
 <!-- vim: set noai ts=4 sw=4: -->
 <?php
 $xml = simplexml_load_file("db.xml");
-echo '<div class="category">Unsettled Matches</div>';
-foreach($xml->category as $category) {
-    foreach($category->game as $game) {
-        if(empty($_GET["game"]) || isset($_GET["game"]) && strcmp($game["id"], $_GET["game"]) == 0) {
-            foreach($game->matches->match as $match) {
-                if(!isset($match->result)) {
-                    echo '<div class="match" onclick=loadContent("bet.php?game='.$game["id"].'&match='.$match["id"].'")>';
-                    echo '  <div class="matchinfo">';
-                    echo '      <div class="matchdetail">'.$match->date.'</div>';
-                    echo '      <img src="'.$match->team[0]->icon.'" alt=""/>';
-                    echo '      '.$match->team[0]["name"].' vs. '.$match->team[1]["name"];
-                    echo '      <img src="'.$match->team[1]->icon.'" alt=""/>';
-                    echo '      <div class="matchdetail">'.$game["name"].'</div>';
-                    echo '  </div>';
-                    echo '</div>';
-                }
-            }
-        }
-    }
-}
-echo '<div class="category">Settled Matches</div>';
+$upcoming = array();
+$latest = array();
 foreach($xml->category as $category) {
     foreach($category->game as $game) {
         if(empty($_GET["game"]) || isset($_GET["game"]) && strcmp($game["id"], $_GET["game"]) == 0) {
             foreach($game->matches->match as $match) {
                 if(isset($match->result)) {
-                    echo '<div class="match" onclick=loadContent("bet.php?game='.$game["id"].'&match='.$match["id"].'")>';
-                    if(strcmp($match->result, "0") == 0) {
-                        echo '  <div class="side0g">'.$match->team[0]->score.'</div><div class="arrow0g"></div>';
-                    } elseif(strcmp($match->result, "1") == 0) {
-                        echo '  <div class="side0r">'.$match->team[0]->score.'</div><div class="arrow0r"></div>';
-                    } elseif(strcmp($match->result, "tie") == 0) {
-                        echo '  <div class="side0">'.$match->team[0]->score.'</div><div class="arrow0"></div>';
-                    }
-                    echo '  <div class="matchinfo">';
-                    echo '      <div class="matchdetail">'.$match->date.'</div>';
-                    echo '      <img src="'.$match->team[0]->icon.'" alt=""/>';
-                    echo '      '.$match->team[0]["name"].' vs. '.$match->team[1]["name"];
-                    echo '      <img src="'.$match->team[1]->icon.'" alt=""/>';
-                    echo '      <div class="matchdetail">'.$game["name"].'</div>';
-                    echo '  </div>';
-                    if(strcmp($match->result, "0") == 0) {
-                        echo '  <div class="side1r">'.$match->team[1]->score.'</div><div class="arrow1r"></div>';
-                    } elseif(strcmp($match->result, "1") == 0) {
-                        echo '  <div class="side1g">'.$match->team[1]->score.'</div><div class="arrow1g"></div>';
-                    } elseif(strcmp($match->result, "tie") == 0) {
-                        echo '  <div class="side1">'.$match->team[1]->score.'</div><div class="arrow1"></div>';
-                    }
-                    echo '</div>';
+                    $latest[] = array("g" => $game, "m" => $match);
+                } else {
+                    $upcoming[] = array("g" => $game, "m" => $match);
                 }
             }
         }
     }
+}
+echo '<div class="category">Upcoming Matches</div>';
+usort($upcoming, function($m1, $m2) {
+    return strcmp($m1["m"]->date,$m2["m"]->date);
+});
+foreach($upcoming as $match) {
+    echo '<div class="match" onclick=loadContent("bet.php?game='.$match["g"]["id"].'&match='.$match["m"]["id"].'")>';
+    echo '  <div class="matchinfo">';
+    echo '      <div class="matchdetail">'.$match["m"]->date.'</div>';
+    echo '      <img src="'.$match["m"]->team[0]->icon.'" alt=""/>';
+    echo '      '.$match["m"]->team[0]["name"].' vs. '.$match["m"]->team[1]["name"];
+    echo '      <img src="'.$match["m"]->team[1]->icon.'" alt=""/>';
+    echo '      <div class="matchdetail">'.$match["g"]["name"].'</div>';
+    echo '  </div>';
+    echo '</div>';
+}
+echo '<div class="category">Latest Matches</div>';
+usort($latest, function($m1, $m2) {
+    return strcmp($m2["m"]->date,$m1["m"]->date);
+});
+foreach($latest as $match) {
+    if(strcmp($match["m"]->result, "0") == 0) {
+        $c0 = "g";
+        $c1 = "r";
+    } elseif(strcmp($match["m"]->result, "1") == 0) {
+        $c0 = "r";
+        $c1 = "g";
+    } elseif(strcmp($match["m"]->result, "tie") == 0) {
+        $c0 = "";
+        $c1 = "";
+    }
+    echo '<div class="match">';
+    echo '  <div class="side0'.$c0.'">'.$match["m"]->team[0]->score.'</div><div class="arrow0'.$c0.'"></div>';
+    echo '  <div class="matchinfo">';
+    echo '      <div class="matchdetail">'.$match["m"]->date.'</div>';
+    echo '      <img src="'.$match["m"]->team[0]->icon.'" alt=""/>';
+    echo '      '.$match["m"]->team[0]["name"].' vs. '.$match["m"]->team[1]["name"];
+    echo '      <img src="'.$match["m"]->team[1]->icon.'" alt=""/>';
+    echo '      <div class="matchdetail">'.$match["g"]["name"].'</div>';
+    echo '  </div>';
+    echo '  <div class="side1'.$c1.'">'.$match["m"]->team[1]->score.'</div><div class="arrow1'.$c1.'"></div>';
+    echo '</div>';
 }
 ?>
