1f0bb5de353909bb738fd189b9ff3cac3f1eb161
Serede Sixty Six <serede.dev@gmail.com>
2016-10-25 18:28:45 +0200

    Credit pages on progress.

--- p2/credit.php
+++ p2/credit.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<div class="title">
+    Credit
+</div>
 <?php
 session_start();
 if(isset($_SESSION["user"])) {
@@ -8,14 +11,41 @@ if(isset($_SESSION["user"])) {
         if(!$data) {
             echo '<div class="error">Corrupt user.</div>';
         } else {
-            echo '<div class="title">Current credit:</div>';
-            echo '<div class="category">'.$data[5].' €</div>';
-            echo '<div class="option">';
-            echo '  Charge credit';
-            echo '</div>';
-            echo '<div class="option">';
-            echo '  Withdraw credit';
-            echo '</div>';
+            if(isset($_GET["option"])) {
+                if(strcmp($_GET["option"], "charge") == 0) {
+                    if(isset($_GET["1"])) {
+
+                    } else {
+                        echo '<div class="text">Charge credit</div>';
+                        echo '<form method="post" name="chrgform" onsubmit="return false">';
+                        echo '  Amount: <span class="input-euro"><input id="amount" type="number" min="10" max="1000000" step="1" value="1000"></span><br>';
+                        echo "  <input type='reset' value='Back' onclick=loadContent()>";
+                        echo "  <input type='submit' value='Confirm'>";
+                        echo '</div>';
+                    }
+                } elseif(strcmp($_GET["option"], "withdraw") == 0) {
+                    if(isset($_GET["1"])) {
+
+                    } else {
+                        echo '<div class="text">Withdraw credit</div>';
+                        echo '<form method="post" name="wdrwform" onsubmit="return false">';
+                        echo '  Amount: <span class="input-euro"><input id="amount" type="number" min="10" max="'.$data[5].'" step="1" value="10"></span><br>';
+                        echo "  <input type='reset' value='Back' onclick=loadContent()>";
+                        echo "  <input type='submit' value='Confirm'>";
+                        echo '</div>';
+                    }
+                } else {
+                    echo '<div class="error">Invalid option.</div>';
+                }
+            } else {
+                echo '<div class="category">'.$data[5].' €</div>';
+                echo '<div class="option" onclick="loadContent(\'credit.php?option=charge\')">';
+                echo '  Charge credit';
+                echo '</div>';
+                echo '<div class="option" onclick="loadContent(\'credit.php?option=withdraw\')">';
+                echo '  Withdraw credit';
+                echo '</div>';
+            }
         }
         unset($data);
     }
