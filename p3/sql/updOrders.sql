b29b1bddde06227c0c954cde7c3c92658aea9413
Serede Sixty Six <serede.dev@gmail.com>
2016-11-30 16:51:36 +0100

    Please work.

--- p3/sql/updOrders.sql
+++ p3/sql/updOrders.sql
@@ -6,27 +6,19 @@
 create or replace function updorders()
 returns trigger as $$
 begin
-    if TG_OP = 'DELETE' then
-        new := old;
+    if TG_OP = 'INSERT' then
+        update clientorders
+        set totalamount = totalamount + coalesce(new.bet,0), totaloutcome = totaloutcome + coalesce(new.outcome,0)
+        where orderid = new.orderid;
+    elsif TG_OP = 'UPDATE' then
+        update clientorders
+        set totalamount = totalamount - coalesce(old.bet,0) + coalesce(new.bet,0), totaloutcome = totaloutcome - coalesce(old.outcome,0) + coalesce(new.outcome,0)
+        where orderid = new.orderid;
+    else -- DELETE
+        update clientorders
+        set totalamount = totalamount - coalesce(old.bet,0), totaloutcome = totaloutcome - coalesce(old.outcome,0)
+        where orderid = old.orderid;
     end if;
-    -- Se podria utilizar new para calcular
-    -- de nuevo el amount/outcome, sin embargo
-    -- para evitar inconsistencias, se recalcula
-    -- para ese order
-
-    update clientorders
-    set totalamount = 0, totaloutcome = 0
-    where clientorders.orderid = new.orderid;
-
-    update clientorders
-    set totalamount = aux.amount, totaloutcome = aux.outcome
-    from (
-        select sum(coalesce(0,bet)) as amount, sum(coalesce(outcome,0)) as outcome
-        from clientbets
-        where clientbets.orderid = new.orderid
-        group by orderid
-    ) as aux
-    where clientorders.orderid = new.orderid;
 
     return new;
 end; $$
