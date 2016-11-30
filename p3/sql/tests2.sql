e5aa30674d4d70b8f45f6e06ef0ba239a1fc4e90
Daniel Perdices <daniel.perdices@estudiante.uam.es>
2016-11-30 10:49:50 +0100

    Report almost finished. E-R ready and more tests.

---
+++ p3/sql/tests2.sql
@@ -0,0 +1,18 @@
+insert into clientorders (customerid, date, orderid) 
+values (693, NULL, 999999998);
+
+insert into clientbets (optionid, bet, ratio, betid, orderid) 
+values (111, 1000, 6.66, 3920, 999999998);
+
+insert into clientbets (optionid, bet, ratio, betid, orderid) 
+values (110, 1000, 6.66, 3920, 999999998);
+
+update clientorders set date = now() where orderid = 999999998;
+
+select * from clientbets where orderid=999999998;
+select * from clientorders where orderid=999999998;
+
+update bets SET winneropt = 111 where betid = 3920;
+
+select * from clientbets where orderid=999999998;
+select * from clientorders where orderid=999999998;
